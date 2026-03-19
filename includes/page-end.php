14ac77ecda0d5da398127aa18c57680bfd09928c
Santiago Sierra <sansiag94@gmail.com>
2026-03-19 21:33:35 +0100

    Rebuild site as static HTML for GitHub Pages

--- includes/page-end.php
+++
@@ -1,7 +0,0 @@
-<?php require __DIR__ . '/footer.php'; ?>
-<?php foreach ($page['footer_scripts'] as $script): ?>
-  <?php site_render_script_tag($script); ?>
-<?php endforeach; ?>
-<script src="script.js"></script>
-</body>
-</html>
